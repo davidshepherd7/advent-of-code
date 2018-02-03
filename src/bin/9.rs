ec488e248b1d02291166642ad0d17e5035bac768
David Shepherd <davidshepherd7@gmail.com>
2018-02-03 19:03:13 +0000

    Avoid copying in 9

--- src/bin/9.rs
+++ src/bin/9.rs
@@ -1,70 +1,76 @@
 use std::io::{self, Read};
 
+
+type CharIter<'a> = Box<Iterator<Item = char> + 'a>;
+
+
 fn main() {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
+    println!("{:?}", total_score(Box::new(buffer.chars())));
 
-    let clean = strip_garbage(buffer.as_str());
-    println!("{:?}", clean);
-    println!("{:?}", total_score(buffer.as_str()));
+    assert!(total_score(Box::new("{}".chars())).0 == 1);
+    assert!(total_score(Box::new("{{{}}}".chars())).0 == 6);
+    assert!(total_score(Box::new("{{},{}}".chars())).0 == 5);
+    assert!(total_score(Box::new("{{{},{},{{}}}}".chars())).0 == 16);
+    assert!(total_score(Box::new("{<a>,<a>,<a>,<a>}".chars())).0 == 1);
+    assert!(total_score(Box::new("{{<ab>},{<ab>},{<ab>},{<ab>}}".chars())).0 == 9);
+    assert!(total_score(Box::new("{{<!!>},{<!!>},{<!!>},{<!!>}}".chars())).0 == 9);
+    assert!(total_score(Box::new("{{<a!>},{<a!>},{<a!>},{<ab>}}".chars())).0 == 3);
+}
 
+fn strip_cancelled<'a>(data: CharIter<'a>) -> CharIter<'a> {
+    let mut cancelled = false;
+    let out = data
+        .filter(move |&c| {
+            if cancelled {
+                cancelled = false;
+                return false;
+            }
+            else if c == '!' {
+                cancelled = true;
+                return false;
+            }
+            else {
+                return true;
+            }
+        });
 
-    assert!(total_score("{}").0 == 1);
-    assert!(total_score("{{{}}}").0 == 6);
-    assert!(total_score("{{},{}}").0 == 5);
-    assert!(total_score("{{{},{},{{}}}}").0 == 16);
-    assert!(total_score("{<a>,<a>,<a>,<a>}").0 == 1);
-    assert!(total_score("{{<ab>},{<ab>},{<ab>},{<ab>}}").0 == 9);
-    assert!(total_score("{{<!!>},{<!!>},{<!!>},{<!!>}}").0 == 9);
-    assert!(total_score("{{<a!>},{<a!>},{<a!>},{<ab>}}").0 == 3);
+    return Box::new(out);
 }
 
 
-fn strip_garbage(data: &str) -> (String, u64) {
-
-    let mut v = Vec::<char>::new();
+fn strip_garbage<'a>(data: CharIter<'a>) -> (CharIter<'a>, u64) {
     let mut garbage_count = 0;
-
-    let mut skip = false;
     let mut garbage = false;
-    for d in data.chars() {
-        if skip {
-            skip = false;
-            continue;
-        }
-        else if d == '!' {
-            skip = true;
-            continue;
-        }
-        else if garbage {
+    let out = data.filter(move |&d| {
+        if garbage {
             if d == '>' {
                 garbage = false;
-                continue;
             }
             else {
                 garbage_count += 1;
-                continue;
             }
+            return false;
         }
         else if d == '<' {
             garbage = true;
-            continue;
+            return false;
         }
         else {
-            v.push(d);
-            continue;
+            return true;
         }
-    }
+    });
 
-    return ( v.into_iter().collect(), garbage_count );
+    return ( Box::new(out), garbage_count );
 }
 
 
-fn scores(data: &str) -> Vec<u64> {
+fn scores(data: CharIter) -> Vec<u64> {
     let mut out = Vec::<u64>::new();
 
     let mut depth = 0;
-    for d in data.chars() {
+    for d in data {
         if d == '{' {
             depth += 1;
         }
@@ -77,7 +83,8 @@ fn scores(data: &str) -> Vec<u64> {
     return out;
 }
 
-fn total_score(data: &str) -> (u64, u64) {
-    let (clean, garbage_count) = strip_garbage(data);
-    return (scores(clean.as_str()).iter().sum(), garbage_count);
+
+fn total_score(data: CharIter) -> (u64, u64) {
+    let (clean, garbage_count) = strip_garbage(strip_cancelled(data));
+    return (scores(clean).iter().sum(), garbage_count);
 }

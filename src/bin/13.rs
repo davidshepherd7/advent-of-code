f74c694849961db008e25159ca7ab5a82661055b
David Shepherd <davidshepherd7@gmail.com>
2018-03-01 15:07:54 +0000

    Custom iterator version of day 13

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -5,6 +5,7 @@ use std::io::{self, Read};
 
 use std::collections::HashMap;
 
+type Firewall = HashMap<usize, usize>;
 
 #[derive(Debug)]
 #[derive(Clone)]
@@ -12,17 +13,34 @@ struct State {
     pos: usize,
     caught: bool,
     delay: usize,
+    firewall: Firewall,
+    size: usize,
 }
 
-type Firewall = HashMap<usize, usize>;
+impl Iterator for State {
+    type Item = (usize, bool);
+    fn next(&mut self) -> Option<(usize, bool)> {
+        if self.pos >= self.size {
+            return None;
+        }
+
+        let out = Some((self.pos, self.caught));
+
+        self.pos = self.pos + 1;
+        self.caught = check_if_caught(&self.firewall, self.pos, self.delay);
+
+        return out;
+    }
+}
 
 impl State {
     fn new(firewall: &Firewall, delay: usize) -> State {
-        let position = 0;
         return State {
-            pos: position,
-            caught: check_if_caught(&firewall, position, delay),
+            pos: 0,
+            caught: check_if_caught(&firewall, 0, delay),
             delay: delay,
+            firewall: firewall.clone(),
+            size: *firewall.keys().max().unwrap() + 1,
         };
     }
 }
@@ -34,48 +52,24 @@ fn check_if_caught(firewall: &Firewall, current_position: usize, delay: usize) -
     };
 }
 
-
-fn step(prev: &State, firewall: &Firewall) -> State {
-    return State {
-        pos: prev.pos + 1,
-        caught: check_if_caught(&firewall, prev.pos + 1, prev.delay),
-        delay: prev.delay,
-    };
-}
-
-fn caught(s: &&State) -> bool {
-    return s.caught;
-}
-
-fn cost(s: &State, firewall: &Firewall) -> usize {
-    match firewall.get(&s.pos) {
-        Some(range) => range * s.pos,
-        None => 0,
-    }
-}
-
-fn run_simulation(firewall: &Firewall, delay: usize) -> Vec<State> {
-    let s = State::new(&firewall, delay);
-
-    let mut states = Vec::<State>::new();
-    states.push(s);
-    for _ in 0..*firewall.keys().max().unwrap() {
-        let next = step(&states.last().unwrap(), &firewall);
-        states.push(next);
-    }
-
-    return states;
+fn run_simulation(firewall: &Firewall, delay: usize) -> State {
+    return State::new(&firewall, delay);
 }
 
 fn total_cost(firewall: &Firewall, delay: usize) -> usize {
-    return run_simulation(&firewall, delay).iter()
-        .filter(caught)
-        .map(|x| cost(&x, &firewall))
+    return run_simulation(&firewall, delay)
+        .filter(|&(_, caught)| caught)
+        .map(|(pos, _)|
+             match firewall.get(&pos) {
+                 Some(range) => range * pos,
+                 None => 0,
+             }
+        )
         .sum::<usize>();
 }
 
 fn ever_caught(firewall: &Firewall, delay: usize) -> bool {
-    return run_simulation(&firewall, delay).iter().any(|state| state.caught);
+    return run_simulation(&firewall, delay).any(|(_, caught)| caught);
 }
 
 
@@ -105,6 +99,7 @@ fn main() {
     let firewall: HashMap<usize, usize> = [(0, 3), (1, 2), (4, 4), (6, 4)].iter().cloned().collect();
     println!("{:?}", firewall);
     assert!(total_cost(&firewall, 0) == 24);
+    println!("min delay {:?}", min_delay(&firewall));
     assert!(min_delay(&firewall) == 10);
 
     let mut buffer = String::new();

fe29f58f5fdc27bb3d5607a58b66f8bc25d93381
David Shepherd <davidshepherd7@gmail.com>
2018-03-01 14:46:42 +0000

    Faster version of day 13

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -3,64 +3,43 @@ extern crate itertools;
 
 use std::io::{self, Read};
 
+use std::collections::HashMap;
+
 
 #[derive(Debug)]
 #[derive(Clone)]
 struct State {
-    pos: i64,
-    scanning: Vec<Option<u32>>,
+    pos: usize,
     caught: bool,
+    delay: usize,
 }
 
-type Firewall = Vec<Option<u32>>;
+type Firewall = HashMap<usize, usize>;
 
 impl State {
-    fn new(firewall: &Firewall, delay: u32) -> State {
-        let position = -1 * (delay as i64);
-        let scanning = firewall.iter().map(|f| match f {
-                &Some(_) => Some(0),
-                &None => None
-            }).collect();
+    fn new(firewall: &Firewall, delay: usize) -> State {
+        let position = 0;
         return State {
             pos: position,
-            caught: check_if_caught(&scanning, position),
-            scanning: scanning,
+            caught: check_if_caught(&firewall, position, delay),
+            delay: delay,
         };
     }
 }
 
-fn progress_layer(currently_scanning: &Option<u32>, range: &Option<u32>) -> Option<u32> {
-    if currently_scanning.is_none() || range.is_none() {
-        return None;
-    }
-    else {
-        let location = currently_scanning.clone().unwrap();
-        return Some((location + 1) % (2* (range.unwrap() - 1)));
-    }
-}
-
-fn check_if_caught(scanning: &Vec<Option<u32>>, current_position: i64) -> bool {
-    if current_position < 0 {
-        return false;
-    }
-    else {
-        let t: usize = current_position as usize;
-        return !scanning[t].is_none()
-            && scanning[t].clone().unwrap() == 0;
-    }
+fn check_if_caught(firewall: &Firewall, current_position: usize, delay: usize) -> bool {
+    return match firewall.get(&current_position) {
+        Some(range) => (current_position + delay) % (2 * (range - 1)) == 0,
+        None => false,
+    };
 }
 
 
 fn step(prev: &State, firewall: &Firewall) -> State {
-    let scanning: Vec<Option<u32>> = prev.scanning.iter()
-        .cloned()
-        .zip(firewall)
-        .map(|(s, f)| progress_layer(&s, &f))
-        .collect();
     return State {
         pos: prev.pos + 1,
-        caught: check_if_caught(&scanning, prev.pos + 1),
-        scanning: scanning,
+        caught: check_if_caught(&firewall, prev.pos + 1, prev.delay),
+        delay: prev.delay,
     };
 }
 
@@ -68,46 +47,39 @@ fn caught(s: &&State) -> bool {
     return s.caught;
 }
 
-fn cost(s: &State, firewall: &Firewall) -> u32 {
-    assert!(s.pos >= 0, "Cannot be caught at negative positions but postion is '{:?}'", s.pos);
-    if let Some(range) = firewall[s.pos as usize] {
-        return range * (s.pos as u32);
-    }
-    else {
-        return 0;
+fn cost(s: &State, firewall: &Firewall) -> usize {
+    match firewall.get(&s.pos) {
+        Some(range) => range * s.pos,
+        None => 0,
     }
 }
 
-fn run_simulation(firewall: &Firewall, delay: u32) -> Vec<State> {
+fn run_simulation(firewall: &Firewall, delay: usize) -> Vec<State> {
     let s = State::new(&firewall, delay);
 
     let mut states = Vec::<State>::new();
     states.push(s);
-    for _ in 1..(firewall.len() + delay as usize) {
-        let prev = &states.last().cloned().unwrap();
-        states.push(step(&prev, &firewall));
-
-        if states.last().unwrap().caught {
-            println!("caught at {:?}", states.last().unwrap().pos);
-        }
+    for _ in 0..*firewall.keys().max().unwrap() {
+        let next = step(&states.last().unwrap(), &firewall);
+        states.push(next);
     }
 
     return states;
 }
 
-fn total_cost(firewall: &Firewall, delay: u32) -> u32 {
+fn total_cost(firewall: &Firewall, delay: usize) -> usize {
     return run_simulation(&firewall, delay).iter()
         .filter(caught)
         .map(|x| cost(&x, &firewall))
-        .sum::<u32>();
+        .sum::<usize>();
 }
 
-fn ever_caught(firewall: &Firewall, delay: u32) -> bool {
+fn ever_caught(firewall: &Firewall, delay: usize) -> bool {
     return run_simulation(&firewall, delay).iter().any(|state| state.caught);
 }
 
 
-fn min_delay(firewall: &Firewall) -> u32 {
+fn min_delay(firewall: &Firewall) -> usize {
     let delays = 0..;
     return delays
         .map(|d|{ println!("{:?}", d); return d;})
@@ -119,11 +91,10 @@ fn min_delay(firewall: &Firewall) -> u32 {
 
 fn parse(data: &str) -> Firewall {
     let mut out = Firewall::new();
-    out.resize(85, None);
 
     for line in data.split("\n").filter(|&l| l != "") {
         let l: Vec<&str> = line.split(": ").collect();
-        out[l[0].parse::<usize>().unwrap()] = Some(l[1].parse::<u32>().unwrap());
+        out.insert(l[0].parse::<usize>().unwrap(), l[1].parse::<usize>().unwrap());
     }
 
     return out;
@@ -131,7 +102,8 @@ fn parse(data: &str) -> Firewall {
 
 
 fn main() {
-    let firewall = vec![Some(3), Some(2), None, None, Some(4), None, Some(4)];
+    let firewall: HashMap<usize, usize> = [(0, 3), (1, 2), (4, 4), (6, 4)].iter().cloned().collect();
+    println!("{:?}", firewall);
     assert!(total_cost(&firewall, 0) == 24);
     assert!(min_delay(&firewall) == 10);
 
